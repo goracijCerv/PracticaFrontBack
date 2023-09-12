0584701b8156333f258c56c847a285a29e83367a
goracijCerv <100542978+goracijCerv@users.noreply.github.com>
2023-09-12 11:54:26 -0600

    Se agrego el proyect de api, se agrego el controlador y la clase del mismo

---
+++ BookApi/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using BookApi.Entitys;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book
+                {
+                    id =1,
+                    title="Las aventuras flipantes de vegeta",
+                    author="Vegata 777",
+                    sales=4001
+                }
+            };
+        }
+    }
+}
---
+++ BookApi/Entitys/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookApi.Entitys
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string title { get; set; } = string.Empty;
+        public string author { get; set; } = string.Empty;
+        public DateTime datePublication { get; set; } = DateTime.Today;
+        public int sales { get; set; }
+    }
+}
