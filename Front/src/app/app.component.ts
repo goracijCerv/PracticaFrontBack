7373fceab8cfcd2d2b3b15e8974e73b57306c814
goracijCerv <100542978+goracijCerv@users.noreply.github.com>
2023-09-12 10:38:18 -0600

    Se creo el modelo de libro, además de empezar a usar el servicio

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/models/book';
+import { BooksService } from 'src/services/books.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'Front';
+  books: Book[] = []
+
+  constructor(private bookService: BooksService) {
+    
+  }
+
+  ngOnInit(): void {
+    this.books = this.bookService.getBooks();
+    console.log(this.books);
+  }
 }
