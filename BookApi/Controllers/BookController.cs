4af2bc7a8be88b8b203128a0916d7fa361b3d9c3
goracijCerv <100542978+goracijCerv@users.noreply.github.com>
2023-09-13 12:19:53 -0600

    Se agregaron mas metodos al controlodador

--- BookApi/Controllers/BookController.cs
+++ BookApi/Controllers/BookController.cs
@@ -1,6 +1,9 @@
-﻿using BookApi.Entitys;
+﻿using BookApi.Data;
+using BookApi.Entitys;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
 
 namespace BookApi.Controllers
 {
@@ -8,19 +11,61 @@ namespace BookApi.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public BookController(DataContext context) {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<Book>>> GetBooks()
         {
-            return new List<Book>
-            {
-                new Book
-                {
-                    id =1,
-                    title="Las aventuras flipantes de vegeta",
-                    author="Vegata 777",
-                    sales=4001
-                }
-            };
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+                return BadRequest("El libro no existe");
+            return Ok(book);
+        }
+
+        [HttpPost("add-book")]
+        public async Task<ActionResult<List<Book>>> AddBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Book>>> UpdateBook(Book book)
+        {
+            var booko =  await _context.Books.FindAsync(book.id);
+            if (booko == null)
+                return BadRequest("No existe este libro");
+            booko.title = book.title;
+            booko.author = book.author;
+            booko.datePublication = book.datePublication;
+            booko.sales = book.sales;
+
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        [HttpDelete("d/{id}")]
+        public async Task<ActionResult<List<Book>>> DelateBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+                return BadRequest("No existe este libro");
+            
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Books.ToListAsync());
         }
     }
 }
---
+++ BookApi/Migrations/20230913164801_creacionBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class creacionBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    title = table.Column<string>(type: "TEXT", nullable: false),
+                    author = table.Column<string>(type: "TEXT", nullable: false),
+                    datePublication = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    sales = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
